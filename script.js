e0b3fdd0ca4eae102fa32edc9b73817aad3d08ae
melaniehey <melanie-kirchkesner@gmx.de>
2022-12-09 17:48:11 +0100

    worked on navigation and page 1
    hintergrund 1, sonnencreme foto, text, naviation, button

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+
 // off canvas für burgermenü
 function openSidenav() {
     document.getElementById("mysidenav").style.width = "100%";
@@ -18,4 +21,9 @@ function openSidenav() {
   function closeSidenav2() {
     document.getElementById("mysidenav2").style.width = "0";
     document.getElementById("navbar").style.marginRight= "0";
+  }
+
+  // button färbt bei onclick die farbe
+  function changeColor() {
+    document.getElementById("abc").style.color= "red";
   }
