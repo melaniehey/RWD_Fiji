0b7f44a0d3b3df40a59764b1d5d8b8297832a310
melaniehey <melanie-kirchkesner@gmx.de>
2022-12-26 22:03:07 +0100

    smol fixes
    container, nav, newsletter form, footer aufzählungen, rand, buttons, tried sticky color changing nav

---
+++ script.js
@@ -0,0 +1,14 @@
+// https://codepen.io/javedkhanui/pen/VXPxPw
+
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         var scroll = $(window).scrollTop();
+//         if (scroll > 300) {
+//           $(".black").css("background" , "blue");
+//         }
+  
+//         else{
+//             $(".black").css("background" , "#333");  	
+//         }
+//     })
+//   })
